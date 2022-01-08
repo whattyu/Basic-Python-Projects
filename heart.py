cb2d69aaa678ab005ad74af9f2af54cd0b106217
whattyu <71334131+whattyu@users.noreply.github.com>
2022-01-08 13:50:03 +0300

    Create heart.py

---
+++ heart.py
@@ -0,0 +1,23 @@
+import turtle
+import time
+
+size = 2
+x, y = turtle.position()
+turtle.penup()
+turtle.setposition(x, y - size*85)
+turtle.pendown()
+turtle.color('red')
+turtle.begin_fill()
+turtle.pensize(3)
+turtle.left(50)
+turtle.forward(133*size)
+turtle.circle(50*size, 200)
+turtle.right(140)
+turtle.circle(50*size, 200)
+turtle.forward(133*size)
+turtle.end_fill()
+time.sleep(2)
+
+"""
+I'll love the light for it shows me the way, yet I'll endure the darkness because it shows me the stars.
+"""
